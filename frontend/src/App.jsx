32145bbf4c0580bea529e0dccf8ea8ab608e326d
harukatou <bqtrung1001@gmail.com>
2025-03-19 20:06:33 +0700

    summarization: initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Sidebar from "./components/SideBar";
 import NewestNews from "./functions/NewestNews";
 import "./App.css"
